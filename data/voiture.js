b81638d56e3a4ffcee1db7ae281c6f7be3a46ce6
wiammatat <matat.wiam.mw@example.com>
2025-11-05 12:27:03 +0100

    Modifier add/find et mise à jour du tableau de voitures

---
+++ data/voiture.js
@@ -0,0 +1,6 @@
+// data/products.js
+export default [
+  { id: 1, marque: "Toyota", modele: "Corolla", annee: 2018, prix: 12000, createdAt: "2019-01-15T10:00:00Z" },
+  { id: 2, marque: "Renault", modele: "Clio", annee: 2020, prix: 14000, createdAt: "2020-04-20T09:30:00Z" },
+  { id: 3, marque: "Peugeot", modele: "208", annee: 2019, prix: 13500, createdAt: "2019-07-10T14:20:00Z" }
+];
--- main.js
+++ main.js
@@ -1,18 +1,53 @@
-const delay = (ms) => new Promise(res => setTimeout(res, ms));
+import { PRODUCTS } from "./data/products.js"; 
+import { isExpired } from "./utils/date.js"; 
+import { Voiture } from "./data/voiture.js";
 
-const addProduct = async (list, newP) => {
-  await delay(300);
-  const id = Math.max(...list.map(p => p.id)) + 1;
-  return [...list, { id, ...newP }];
-};
 
-const newList = await addProduct(PRODUCTS, {
-  name: "Savon",
-  category: "Hygiène",
-  price: 5,
-  quantity: 10,
-  expiryDate: "2026-01-01",
-  tags: ["hygiene"]
-});
+const expired = PRODUCTS.filter(p => isExpired(p.expiryDate)); 
+console.log("Produits expirés :", expired);
+ 
+PRODUCTS.forEach((p) => console.log(p));
+PRODUCTS.forEach((p) => console.log(p.name)); 
 
+
+const total = PRODUCTS.reduce( 
+(somme, p) => somme + p.price * p.quantity, 
+0 
+); 
+console.log("Valeur totale du stock :", total, "MAD");
+
+const promo = PRODUCTS.filter(p => p.tags.includes("promo")); 
+console.log("Produits en promo :", promo.map(p => p.name)); 
+const sorted = [...PRODUCTS].sort((a, b) => a.price - b.price); 
+console.log("Tri par prix croissant :", sorted.map(p => 
+p.name));
+
+
+const delay = (ms) => new Promise(res => setTimeout(res, ms)); 
+const addProduct = async (list, newP) => { 
+await delay(300);
+const id = Math.max(...list.map(p => p.id)) + 1; 
+return [...list, { id, ...newP }]; 
+}; 
+const newList = await addProduct(PRODUCTS, { 
+name: "Savon", 
+category: "Hygiène", 
+price: 5, 
+quantity: 10, 
+expiryDate: "2026-01-01", 
+tags: ["hygiene"] 
+}); 
 console.log("Après ajout :", newList.length, "produits");
+
+
+
+Voiture.forEach(v=>console.log(v));
+Voiture.push({ 
+    id: 4, 
+    name: "tyara",  
+    price: 25, 
+    currency: "MAD", 
+    quantity: 15, 
+  });
+Voiture.pop();
+  Voiture.forEach(v=>console.log(v));
